d300b4592c57586c6a9b3ad48f7edd8fcb211bdc
majky <44968533+dzubidas@users.noreply.github.com>
2024-06-15 15:46:09 +0200

    Create update_lando_local.sh
    lando update script

---
+++ update_lando_local.sh
@@ -0,0 +1,9 @@
+lando composer self-update
+lando composer install
+#lando composer update --lock
+# pokud hlasi outdated lock
+lando drush cr
+lando drush cim -y
+lando drush cr
+lando drush updb
+lando drush cr
